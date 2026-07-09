65aae8954e2b3bbbe6c34de680df6ee060e25c90
ksns25 <33980755+Neil100@users.noreply.github.com>
2026-07-09 21:17:30 +0530

    Add: copy-linked-list-with-random-pointer - submission-3

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-3.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        // Node current = head, previous;
+        // Node dummy = new Node(0);
+        // previous = dummy;
+        // while(current != null) {
+        //     Node newCurrent = new Node(current.val);
+
+        //     previous.next = newCurrent;
+        //     map.put(current, newCurrent);
+
+        //     previous = previous.next;
+        //     current = current.next;
+        // }
+        // current = head;
+        // Node newCurrent = dummy.next;
+
+        // while(current != null) {
+        //     Node newRandom = map.get(current.random);
+
+        //     newCurrent.random = newRandom;
+
+        //     current = current.next;
+        //     newCurrent = newCurrent.next;
+        // }
+
+        // return dummy.next;
+
+        return recurssion(head, map);
+    }
+
+    Node recurssion(Node head, Map<Node, Node> map) {
+        if(head == null)
+            return null;
+        
+        Node current = new Node(head.val);
+        map.put(head, current);
+        current.next = recurssion(head.next, map);
+
+        current.random = map.get(head.random);
+
+        return current;
+    }
+}
