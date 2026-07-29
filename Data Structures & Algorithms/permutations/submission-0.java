f79991f8e9c6dccb76aa650e15649bf0690e082d
ksns25 <33980755+Neil100@users.noreply.github.com>
2026-07-29 22:30:52 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> sol;
+    int n;
+    public List<List<Integer>> permute(int[] nums) {
+        sol = new ArrayList<>();
+        n = nums.length;
+        List<Integer> current = new ArrayList<>();
+        List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        // System.out.println(current.size());
+        findPermutations(current, numsList);
+        return sol;
+    }
+
+    void findPermutations(List<Integer> current, List<Integer> numsList) {
+        if(current.size()==n) {
+            sol.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i=0; i<numsList.size(); i++) {
+            current.add(numsList.get(i));
+            int taken = numsList.get(i);
+            numsList.remove(i);
+            findPermutations(current, numsList);
+
+            numsList.add(i, taken);
+            // System.out.println(numsList.size() + "!!! " + i);
+            current.remove(current.size()-1);
+        }
+    }
+}
